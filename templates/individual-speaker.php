84cb527cecf6bd97af2c13f1bd7ff5dbf0713a91
Andrew Minion <andrew@andrewrminion.com>
2017-03-16 14:57:18 -0400

    add speaker post classes

--- templates/individual-speaker.php
+++ templates/individual-speaker.php
@@ -1,4 +1,4 @@
-<div class="speaker">
+<div class="speaker <?php implode( ' ', get_post_class() ); ?>">
     <p><a href="<?php the_permalink(); ?>"><?php echo wp_get_attachment_image( get_field( 'speaker_image' ), array( 250, 250 ), false, array( 'class' => 'speaker-photo' ) ) ?></a></p>
     <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <p>Talk Title:</p>
