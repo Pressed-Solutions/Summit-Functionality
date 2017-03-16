b742448b190c62d6d9ccabea4ea9a990d1969833
Andrew Minion <andrew@andrewrminion.com>
2017-03-16 15:18:13 -0400

    add more shortcode parameters

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -7,36 +7,8 @@
  */
 add_shortcode( 'speaker_list', 'sf_speaker_list' );
 function sf_speaker_list( $attributes ) {
-    $shortcode_attributes = shortcode_atts( array (
-        'post_type'         => 'summit_speaker',
-        'cat'               => NULL,
-        'category_name'     => NULL,
-        'category__in'      => array(),
-        'tag'               => NULL,
-        'tag_id'            => NULL,
-        'tag__in'           => array(),
-        'tag_slug__in'      => array(),
-        'orderby'           => 'post_title',
-        'order'             => 'ASC',
-        'posts_per_page'    => -1,
-        'offset'            => NULL,
-    ), $attributes );
-
     // set up query
-    $list_args = array(
-        'post_type'         => $shortcode_attributes['post_type'],
-        'cat'               => $shortcode_attributes['cat'],
-        'category_name'     => $shortcode_attributes['category_name'],
-        'category__in'      => $shortcode_attributes['category__in'],
-        'tag'               => $shortcode_attributes['tag'],
-        'tag_id'            => $shortcode_attributes['tag_id'],
-        'tag__in'           => $shortcode_attributes['tag__in'],
-        'tag_slug__in'      => $shortcode_attributes['tag_slug__in'],
-        'orderby'           => $shortcode_attributes['orderby'],
-        'order'             => $shortcode_attributes['order'],
-        'posts_per_page'    => $shortcode_attributes['posts_per_page'],
-        'offset'            => $shortcode_attributes['offset'],
-    );
+    $list_args = parse_shortcode_args( $attributes );
 
     $list_query = new WP_Query( $list_args );
 
@@ -64,36 +36,9 @@ function sf_speaker_list( $attributes ) {
  */
 add_shortcode( 'member_downloads', 'sf_member_downloads' );
 function sf_member_downloads( $attributes ) {
-    $shortcode_attributes = shortcode_atts( array (
-        'post_type'         => 'summit_speaker',
-        'cat'               => NULL,
-        'category_name'     => NULL,
-        'category__in'      => array(),
-        'tag'               => NULL,
-        'tag_id'            => NULL,
-        'tag__in'           => array(),
-        'tag_slug__in'      => array(),
-        'orderby'           => 'post_title',
-        'order'             => 'ASC',
-        'posts_per_page'    => -1,
-        'offset'            => NULL,
-    ), $attributes );
-
     // set up query
-    $downloads_args = array(
-        'post_type'         => $shortcode_attributes['post_type'],
-        'cat'               => $shortcode_attributes['cat'],
-        'category_name'     => $shortcode_attributes['category_name'],
-        'category__in'      => $shortcode_attributes['category__in'],
-        'tag'               => $shortcode_attributes['tag'],
-        'tag_id'            => $shortcode_attributes['tag_id'],
-        'tag__in'           => $shortcode_attributes['tag__in'],
-        'tag_slug__in'      => $shortcode_attributes['tag_slug__in'],
-        'orderby'           => $shortcode_attributes['orderby'],
-        'order'             => $shortcode_attributes['order'],
-        'posts_per_page'    => $shortcode_attributes['posts_per_page'],
-        'offset'            => $shortcode_attributes['offset'],
-        'meta_query'    => array(
+    $downloads_args = array_merge( parse_shortcode_args( $attributes ), array(
+       'meta_query'    => array(
             array(
                 'key'      => 'video_download_url',
                 'compare'  => 'EXISTS',
@@ -103,8 +48,8 @@ function sf_member_downloads( $attributes ) {
                 'value'    => '',
                 'compare'  => '!=',
             ),
-    ),
-    );
+        ),
+    ));
 
     $downloads_query = new WP_Query( $downloads_args );
 
@@ -123,4 +68,59 @@ function sf_member_downloads( $attributes ) {
     wp_reset_postdata();
 
     return $shortcode_content;
+}
+
+/**
+ * Parse shortcode attributes to WP_Query parameters
+ * @param  array $attributes shortcode attributes
+ * @return array WP_Query parameters
+ */
+function parse_shortcode_args( $attributes ) {
+    $shortcode_attributes = shortcode_atts( array (
+        'post_type'             => 'summit_speaker',
+        'p'                     => NULL,
+        'title'                 => NULL,
+        'pagename'              => NULL,
+        'post_parent'           => NULL,
+        'post_parent__in'       => array(),
+        'post_parent_not__in'   => array(),
+        'post__in'              => array(),
+        'post__not_in'          => array(),
+        'post_name__in'         => array(),
+        'cat'                   => NULL,
+        'category_name'         => NULL,
+        'category__in'          => array(),
+        'tag'                   => NULL,
+        'tag_id'                => NULL,
+        'tag__in'               => array(),
+        'tag_slug__in'          => array(),
+        'orderby'               => 'post_title',
+        'order'                 => 'ASC',
+        'posts_per_page'        => -1,
+        'offset'                => NULL,
+    ), $attributes );
+
+    return array(
+        'post_type'             => $shortcode_attributes['post_type'],
+        'p'                     => $shortcode_attributes['p'],
+        'title'                 => $shortcode_attributes['title'],
+        'pagename'              => $shortcode_attributes['pagename'],
+        'post_parent'           => $shortcode_attributes['post_parent'],
+        'post_parent__in'       => $shortcode_attributes['post_parent__in'],
+        'post_parent_not__in'   => $shortcode_attributes['post_parent_not__in'],
+        'post__in'              => $shortcode_attributes['post__in'],
+        'post__not_in'          => $shortcode_attributes['post__not_in'],
+        'post_name__in'         => $shortcode_attributes['post_name__in'],
+        'cat'                   => $shortcode_attributes['cat'],
+        'category_name'         => $shortcode_attributes['category_name'],
+        'category__in'          => $shortcode_attributes['category__in'],
+        'tag'                   => $shortcode_attributes['tag'],
+        'tag_id'                => $shortcode_attributes['tag_id'],
+        'tag__in'               => $shortcode_attributes['tag__in'],
+        'tag_slug__in'          => $shortcode_attributes['tag_slug__in'],
+        'orderby'               => $shortcode_attributes['orderby'],
+        'order'                 => $shortcode_attributes['order'],
+        'posts_per_page'        => $shortcode_attributes['posts_per_page'],
+        'offset'                => $shortcode_attributes['offset'],
+    );
 }
